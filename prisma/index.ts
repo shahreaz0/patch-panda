d20d34836492b9945b51468c130c39fe01f6ad60
shahreaz0 <ashahreaz@gmail.com>
2025-12-19 22:16:41 +0600

    feat: Integrate Prisma ORM for database management, including schema, migrations, and environment variable setup.

---
+++ prisma/index.ts
@@ -0,0 +1,12 @@
+import { PrismaPg } from "@prisma/adapter-pg"
+import { PrismaClient } from "../lib/generated/prisma/client"
+import "dotenv/config"
+import { env } from "@/lib/env"
+
+const adapter = new PrismaPg({ connectionString: env.DATABASE_URL })
+
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
+
+export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter })
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
